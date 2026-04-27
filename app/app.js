2baea643e290d05a6eb03eec0386802b6b632bd5
zoya9545-web <zoya9545@gmail.com>
2026-04-27 09:08:52 +0530

    test change

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Azure PaaS 🚀');
+  res.end('Hello from Azure PaaS Updated for Azure deployment 🚀🚀');
 });
 
 server.listen(process.env.PORT || 3000);
